8ba2b7b7f0c83a00b5d0ed17bd9ec096525fc50c
royceb <royce.r.brooks@gmail.com>
2020-01-28 18:46:12 -0800

    init

---
+++ src/components/Header.js
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-heart"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Mary & Royce</h1>
+        <p>
+          June 18th, 2020
+          <br></br>
+          San Francisco, CA
+          <br></br>
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Our story
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            RSVP
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            Registry
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
