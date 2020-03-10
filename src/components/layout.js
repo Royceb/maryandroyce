04a39af982fbde7e91690ba03cd6bce99a1b2f6c
royceb <royce.r.brooks@gmail.com>
2020-03-09 20:18:31 -0700

    prevent zoom using gesture listener

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,7 +17,20 @@ const Layout = ({ children, location }) => {
       </div>
     )
   }
+  document.addEventListener('gesturestart', function(e) {
+    e.preventDefault()
+    document.body.style.zoom = 0.99
+  })
 
+  document.addEventListener('gesturechange', function(e) {
+    e.preventDefault()
+
+    document.body.style.zoom = 0.99
+  })
+  document.addEventListener('gestureend', function(e) {
+    e.preventDefault()
+    document.body.style.zoom = 1
+  })
   return (
     <StaticQuery
       query={graphql`
